c2c50bad2b765f551630db29564ed9775de0c35a
Di Wang <diwang@woobo.io>
2021-04-09 13:42:01 -0400

    sync

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-#git clone git://github.com/Digo/dotvim.git ~/.vim
-#git clone ssh://mli-desktop.cs/home/d57wang/Repositories/vim.git ~/.vim
-
-#Create symlinks:
+# Create symlinks:
 ln -s ~/.vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/.backup
 
-git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
-
-
-git config --global github.user Digo
+# Install Fonts
+#brew tap homebrew/cask-fonts
+#brew cask install font-hack-nerd-font
